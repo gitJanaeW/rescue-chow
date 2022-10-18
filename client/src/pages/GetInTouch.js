6f182d0a58f41f2fc747a7ebc7cab81c3409e55b
janae.wel <janae.wel@gmail.com>
2022-10-18 11:50:37 -0400

    added Google Maps

--- client/src/pages/GetInTouch.js
+++ client/src/pages/GetInTouch.js
@@ -51,7 +51,7 @@ const GetInTouch = () => {
                 </div>
             </div>
             <div>
-                {/* Google Maps of business location goes here. I forgot how to do that so I'll have to check */}
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8871.436411564438!2d-78.92307018579231!3d43.87225653063157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d51def76b791b9%3A0xe40b7d9a530a09e0!2s1375%20Hopkins%20St%2C%20Whitby%2C%20ON%20L1N%202C2!5e0!3m2!1sen!2sca!4v1666107343570!5m2!1sen!2sca" title="Rescue Chow Location" width="100%" height="550" style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div>
                 <h2>Send Your Questions</h2>
