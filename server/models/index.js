285dadbea4de5f9245dfd63c6d5994e9b3672b29
janae.wel <janae.wel@gmail.com>
2022-10-18 15:00:16 -0400

    finished models

---
+++ server/models/index.js
@@ -0,0 +1,15 @@
+const Billing = require('./Billing');
+const Guest = require('./Guest');
+const Orders = require('./Orders');
+const Rescues = require('./Rescues');
+const Treats = require('./Treats');
+const User = require('./User');
+
+module.exports = {
+    Billing,
+    Guest,
+    Orders,
+    Rescues,
+    Treats,
+    User
+};
