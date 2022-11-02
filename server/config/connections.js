86e7904fd9acb5b4fc9e0fef0d5d78a301965308
janae.wel <janae.wel@gmail.com>
2022-11-02 15:12:26 -0400

    manual revert of config bug

--- server/config/connections.js
+++ server/config/connections.js
@@ -10,12 +10,3 @@ mongoose.connect(
 );
 
 module.exports = mongoose.connection;
-
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  );
-  
