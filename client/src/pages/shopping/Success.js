80991c6ecdc5ad8bc513dcfc5876935596184a62
Spencer Pichette <ssptuningperformance@gmail.com>
2022-11-02 18:34:53 -0400

    sucess page

--- client/src/pages/shopping/Success.js
+++ client/src/pages/shopping/Success.js
@@ -3,6 +3,7 @@ import { useMutation } from "@apollo/client";
 import Jumbotron from "../../components/shopping/Jumbotron";
 import { ADD_NEW_ORDER } from "../../utils/shopping/mutations";
 import { idbPromise } from "../../utils/helpers";
+import { CheckIcon } from "@heroicons/react/24/outline";
 
 function Success() {
   const [addNewOrder] = useMutation(ADD_NEW_ORDER);
@@ -28,18 +29,43 @@ function Success() {
 
       setTimeout(() => {
         window.location.assign("/");
-      }, 300000);
+      }, 3000);
     }
 
     saveOrder();
   }, [addNewOrder]);
 
   return (
-    <div>
+    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
       <Jumbotron>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
+        <div className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+          <div>
+            <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+              <CheckIcon
+                className="h-6 w-6 text-green-600"
+                aria-hidden="true"
+              />
+            </div>
+            <div className="mt-3 text-center sm:mt-5">
+              <div
+                as="h3"
+                className="text-lg font-medium leading-6 text-gray-900"
+              >
+                Payment successful
+              </div>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                  Thank you for your purchase!
+                </p>
+              </div>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                  You will now be redirected to the Home Page
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </Jumbotron>
     </div>
   );
