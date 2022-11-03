950f1bff11018badad04d6619f578da7e05cfdb7
janae.wel <janae.wel@gmail.com>
2022-11-03 17:45:12 -0400

    fixing heroku

--- server/config/connections.js
+++ server/config/connections.js
@@ -4,8 +4,7 @@ mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rescue-chow',
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-       
+        useUnifiedTopology: true
     }
 );
 
